3c89ace6ca7306007cb628cf694a3d4639b80719
anavilharquide-lgtm <ana.vilharquide@escola.pr.gov.br>
2026-09-25 15:38:51 +0000

    calcular media 1

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function calcularMedia(){
-    const textNota1 = document.getElementById("nota1").value
-    const textNota2 = document.getElementById("nota2").value
-    const textNota3 = document.getElementById("nota3").value
+    const textoNota1 = document.getElementById("nota1").value
+    const textoNota2 = document.getElementById("nota2").value
+    const textoNota3 = document.getElementById("nota3").value
 
-    const textNota1 = Number(textNota1)
-    const textNota2 = Number(textNota2)
-    const textNota3 = Number(textNota3)
+    const nota1 = Number(textoNota1)
+    const nota2 = Number(textoNota2)
+    const nota3 = Number(textoNota3)
 
     const media = (nota1 + nota2 + nota3)/3
     let situacao 
